4c7723a6797367534b3fc79ad368012d4f87574e
jrsperry <joshuasperry@gmail.com>
2018-12-06 16:32:36 -0700

    got percent complete done

--- IntexAzure/IntexAzure/Models/WorkOrdersPrice.cs
+++ IntexAzure/IntexAzure/Models/WorkOrdersPrice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,17 @@ namespace IntexAzure.Models
     public class WorkOrdersPrice
     {
         public WorkOrders WorkOrder { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
         public decimal? WorkOrderPrice { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}", ApplyFormatInEditMode = true)]
         public decimal? CompleteTests { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}", ApplyFormatInEditMode = true)]
         public decimal? InProgressTests { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}", ApplyFormatInEditMode = true)]
         public decimal? BackLogTests { get; set; }
     }
 }
