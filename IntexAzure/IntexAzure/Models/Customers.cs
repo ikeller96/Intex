39fa14bbf6b85e73d93235f504f26f6f1537cf5c
SamuelFaber15 <43383450+SamuelFaber15@users.noreply.github.com>
2018-12-06 10:55:33 -0700

    Customer and work order changes

--- IntexAzure/IntexAzure/Models/Customers.cs
+++ IntexAzure/IntexAzure/Models/Customers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -13,22 +14,47 @@ namespace IntexAzure.Models
         [Key]
         public int CustID { get; set; }
 
+        [StringLength(60, ErrorMessage = "Your Name must be less than 60 characters")]
+        [DisplayName("Name")]
+        [Required(ErrorMessage = "Please enter your name")]
         public string CustName { get; set; }
 
+        [StringLength(30, ErrorMessage = "Your Street Address must be less than 30 characters")]
+        [DisplayName("Street Address")]
+        [Required(ErrorMessage = "Please enter your address")]
         public string CustAddress1 { get; set; }
 
+        [DisplayName("Street Address 2")]
         public string CustAddress2 { get; set; }
 
+        [StringLength(30, ErrorMessage = "Your City must be less than 30 characters")]
+        [DisplayName("City")]
+        [Required(ErrorMessage = "Please enter your City")]
         public string CustCity { get; set; }
 
+        [StringLength(30, ErrorMessage = "Your State must be less than 30 characters")]
+        [DisplayName("State")]
+        [Required(ErrorMessage = "Please enter your State")]
         public string CustState { get; set; }
 
+        [RegularExpression(@"\d{5}", ErrorMessage = "Your ZipCode must be 5 digits")]
+        [DisplayName("Zip Code")]
+        [Required(ErrorMessage = "Please enter your Zip")]
         public int CustZip { get; set; }
 
+        [EmailAddress(ErrorMessage = "Please enter your email address")]
+        [DisplayName("Email")]
+        [Required(ErrorMessage = "Please enter your Email")]
         public string CustEmail { get; set; }
 
-        public string CustPhone { get; set; }
-        
+        //[RegularExpression(@"((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}", ErrorMessage = "Your phone number must have 10 numbers in the format ###-###-#### or (###)###-####")]
+        //[DisplayName("Phone")]
+        //[Required(ErrorMessage = "Please enter your Phone")]
+        //public string CustPhone { get; set; }
+
+        [StringLength(30, ErrorMessage = "Your payment info must be less than 30 characters")]
+        [DisplayName("Payment Info")]
+        [Required(ErrorMessage = "Please enter your payment info")]
         public string CustPaymentInfo { get; set; }
 
         public int? EmpID { get; set; }
@@ -38,8 +64,7 @@ namespace IntexAzure.Models
 
         public string Password { get; set; }
 
-
     }
-
-
 }
+
+
