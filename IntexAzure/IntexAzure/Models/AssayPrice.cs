bbec19f64672741b1717802f975333ebfb04effe
jrsperry <joshuasperry@gmail.com>
2018-12-06 10:32:31 -0700

    added price stuff

---
+++ IntexAzure/IntexAzure/Models/AssayPrice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntexAzure.Models
+{
+    public class AssayPrice
+    {
+        public Assays Assay { get; set; }
+        public decimal EstimatedPrice { get; set; }
+    }
+}
