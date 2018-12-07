52a93ec5cbb2734fd06edde6d7ce6c4823b6c037
Ian Keller <ianwkeller@gmail.com>
2018-12-06 19:04:13 -0700

    fix

--- IntexAzure/IntexAzure/Models/CompoundSamples.cs
+++ IntexAzure/IntexAzure/Models/CompoundSamples.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -10,23 +11,27 @@ namespace IntexAzure.Models
     [Table("CompoundSamples")]
     public class CompoundSamples
     {
+        [DisplayName("Compound LT")]
         public int CompoundLT { get; set; }
 
         [Key]
-        public int CompoundSC { get; set; }       
+        [DisplayName("Compound SC")]
+        public int CompoundSC { get; set; }
 
+        [DisplayName("Quantity")]
         public int Quantity { get; set; }
 
+        [DisplayName("Date Arrived")]
         public DateTime DateArrived { get; set; }
 
+        [DisplayName("Received By")]
         public string ReceivedBy { get; set; }
 
+        [DisplayName("Appearance")]
         public string Appearance { get; set; }
 
+        [DisplayName("Weight")]
         public decimal Weight { get; set; }
-
-
-
 
     }
 }
